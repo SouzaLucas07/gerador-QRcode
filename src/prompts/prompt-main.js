31a382de4fd2ecb1c919007c2e61e8770b2bbd93
lucaspenteadosouza07@gmail.com <lucaspenteadosouza07@gmail.com>
2025-03-18 13:09:19 -0300

    import prompt

---
+++ src/prompts/prompt-main.js
@@ -0,0 +1,9 @@
+const mainPrompt = [
+    {
+    name: "select",
+    description: "Esolha a ferramenta (1 - QRCODE ou 2 - PASSWORD)",
+    pattern: /^[1-2]+$/,
+    message: "Escolha entre 1 e 2",
+    required: true,
+    }
+]
